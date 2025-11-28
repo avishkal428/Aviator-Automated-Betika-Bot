7e49eb6a517945bcc13aa401adef3500431b1420
avishkal428 <avishkal907@gmail.com>
2025-11-28 08:52:14 +0530

    Update SESSION_ID to new value

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "mcl02CQD#ZBzNjgire_t29Ea5_2EQLbVa-ZCvFxK-F8t8YGOw2Mk",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=9a8368c67ab55efa",
 ALIVE_IMG: process.env.ALIVE_IMG || "https://raw.githubusercontent.com/Manju362/Link-gamu./refs/heads/main/IMG-20250408-WA0003.jpg",
 ALIVE_MS: process.env.ALIVE_MSG || "I'm Alive Now",
 };
